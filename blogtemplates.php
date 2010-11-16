7358838cb4921218a49125feca2c4a1b914df4df
ulrich <ulrich@f53fe46e-8c02-459b-9fdd-93e75771b278>
2010-11-16 20:15:26 +0000

    Plugin Blog Templates: Changelog added Bug fix: admin page returning content before display Bug fix: blog template admin email exported to new blogs
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/blogtemplates@1056 f53fe46e-8c02-459b-9fdd-93e75771b278

--- blogtemplates.php
+++ blogtemplates.php
@@ -3,13 +3,13 @@
 Plugin Name: New Blog Templates
 Plugin URI: http://premium.wpmudev.org/project/new-blog-template
 Description: Allows the site admin to create new blogs based on templates, to speed up the blog creation process
-Author: Jason DeVelvis (Incsub)
-Version: 1.1.1
+Author: Jason DeVelvis, Ulrich Sossou (Incsub)
+Version: 1.2.2
 Site Wide Only: true
 Network: true
 WDP ID: 130
-*/   
-   
+*/
+
 /*  Copyright 2010 Incsub (http://incsub.com)
 
 This program is free software; you can redistribute it and/or modify
@@ -28,4 +28,3 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 require_once('blogtemplatesfiles/blog_templates.php');
-?>
