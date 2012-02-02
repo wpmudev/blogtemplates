6b0c3672fe45e74cacd47b003e7362d018ad96df
vladislav <vladislav@f53fe46e-8c02-459b-9fdd-93e75771b278>
2012-02-02 14:39:40 +0000

    Version 1.5
    - Added optional templates selection on registration.
    
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/blogtemplates@5162 f53fe46e-8c02-459b-9fdd-93e75771b278

---
+++ blogtemplatesfiles/template/blog_templates-registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copy this file into your theme directory and edit away!
+ * You can also use $templates array to iterate through your templates.
+ */
+?>
+<?php if (defined('BP_VERSION') && 'bp-default' == get_blog_option(bp_get_root_blog_id(), 'stylesheet')) echo '<br style="clear:both" />'; ?>
+<div id="blog_template-selection">
+	<p class="blog_template-option">
+		<label for="blog_template"><?php _e('Select a template', 'blog_templates') ?></label>
+		<?php
+			// Print templates dropdown selection box.
+			// Pass false as second argument to force a template to be selected.
+			$this->get_template_dropdown('blog_template', true);
+		?>
+	</p>
+</div>
