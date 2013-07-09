ab56d6ba3c5acb2b8f236873896761164dd8985b
ignacio <ignacio@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-07-09 12:46:46 +0000

    Improved the search by name setting
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/blogtemplates@9679 f53fe46e-8c02-459b-9fdd-93e75771b278

--- blogtemplatesfiles/assets/js/nbt-settings.js
+++ blogtemplatesfiles/assets/js/nbt-settings.js
@@ -57,21 +57,32 @@ jQuery(document).ready(function($) {
 			action: 'nbt_get_sites_search',
 			term: request.term
 		};
-		
-		//$.post( export_to_text_js.ajaxurl, data, function( data, status, xhr ) {
+
+
+	    $.ajax({
+			url: export_to_text_js.ajaxurl,
+			data: data,
+			type: 'post',
+			dataType: 'json'
+		}).done(function( data ) {
+			nbt_cache[ term ] = data;
+			response( data );
+		});
+
+	    //$.getJSON( export_to_text_js.ajaxurl, data ).done( function( data, status, xhr ) {
 	    //	console.log(data);
 		//	//nbt_cache[ term ] = data;
 		//	//response( data );
 	    //});
-	    
-
-	    $.getJSON( export_to_text_js.ajaxurl, data ).done( function( data, status, xhr ) {
-	    	console.log(data);
-			//nbt_cache[ term ] = data;
-			//response( data );
-	    });
+	  },
+	  response: function( event, ui ) {
+	  	for ( var i = 0; i < ui.content.length; i++ ) {
+	  		ui.content[i].label = ui.content[i].path + ' [' + ui.content[i].blog_name + ']';
+	  		ui.content[i].value = ui.content[i].blog_name;
+	  	}
+	  },
+	  select: function ( event, ui ) {
+	  	$( '#copy_blog_id' ).val( ui.item.blog_id );
 	  }
 	});
-
-
 });
--- blogtemplatesfiles/blog_templates_admin_pages.php
+++ blogtemplatesfiles/blog_templates_admin_pages.php
@@ -2,30 +2,43 @@
 
 function nbt_get_sites_search() {
 	global $wpdb, $current_site;
-	var_dump($_REQUEST);
+
 	if ( ! empty( $_POST['term'] ) ) 
 		$term = $_REQUEST['term'];
 	else
 		echo json_encode( array() );
-//
-	//$s = isset( $_REQUEST['term'] ) ? stripslashes( trim( $_REQUEST[ 'term' ] ) ) : '';
-	//$wild = '';
-	//if ( false !== strpos($s, '*') ) {
-	//	$wild = '%';
-	//	$s = trim($s, '*');
-	//}
-//
-	//$like_s = esc_sql( like_escape( $s ) );
-	//$query = "SELECT * FROM {$wpdb->blogs} WHERE site_id = '{$wpdb->siteid}' ";
-//
-	//if ( $like_s != trim('/', $current_site->path) )
-	//	$blog_s = $current_site->path . $like_s . $wild . '/';
-	//else
-	//	$blog_s = $like_s;
-//
-	//$query .= " AND  ( {$wpdb->blogs}.path LIKE '$blog_s' ) LIMIT 10";
-$query = "AAAA";
-	echo json_encode( array( 0 => $query ) );
+
+	$s = isset( $_REQUEST['term'] ) ? stripslashes( trim( $_REQUEST[ 'term' ] ) ) : '';
+	$wild = '%';
+	if ( false !== strpos($s, '*') ) {
+		$wild = '%';
+		$s = trim($s, '*');
+	}
+
+	$like_s = esc_sql( like_escape( $s ) );
+	$query = "SELECT * FROM {$wpdb->blogs} WHERE site_id = '{$wpdb->siteid}' ";
+
+	if ( $like_s != trim('/', $current_site->path) )
+		$blog_s = $current_site->path . $like_s . $wild . '/';
+	else
+		$blog_s = $like_s;
+
+	$query .= " AND  ( {$wpdb->blogs}.path LIKE '$blog_s' ) LIMIT 10";
+	$results = $wpdb->get_results( $query );
+
+	$returning = array();
+	if ( ! empty( $results ) ) {
+		foreach ( $results as $row ) {
+			$details = get_blog_details( $row->blog_id );
+			$returning[] = array( 
+				'blog_name' => $details->blogname,
+				'path' => $row->path, 
+				'blog_id' => $row->blog_id 
+			);
+		}
+	}
+
+	echo json_encode( $returning );
 
 	die();
 }
@@ -186,10 +199,11 @@ class blog_templates_admin_pages {
 			                <?php $this->render_row( __( 'Template Name:', $this->localization_domain ), ob_get_clean() ); ?>
 
 			                <?php ob_start(); ?>
-			                    <input name="copy_blog_id" type="text" id="copy_blog_id" class="small-text"/>
+			                    <input name="copy_blog_id" type="text" id="copy_blog_id" class="small-text" placeholder="<?php _e( 'Blog ID', $this->localization_domain ); ?>"/>
 			                    <div class="ui-widget">
-				                    <label for="search_for_blog"> <?php _e( 'Or search by a blog name', $this->localization_domain ); ?> 
+				                    <label for="search_for_blog"> <?php _e( 'Or search by blog path', $this->localization_domain ); ?> 
 										<input type="text" id="search_for_blog" class="medium-text">
+										<span class="description"><?php _e( 'For example, if the blog you are searching has an URL like http://ablog.mydomain.com, you can type "ablog"', $this->localization_domain ); ?></span>
 				                    </label>
 				                </div>
 			                <?php $this->render_row( __( 'Blog ID:', $this->localization_domain ), ob_get_clean() ); ?>
