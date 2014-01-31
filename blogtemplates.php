33879c090723a64ba347b1a84d0f2aebf151d4d8
ignacio <ignacio@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-01-31 17:47:36 +0000

    Changed authorship
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/blogtemplates@12739 f53fe46e-8c02-459b-9fdd-93e75771b278

--- blogtemplates.php
+++ blogtemplates.php
@@ -3,7 +3,7 @@
 Plugin Name: New Blog Templates
 Plugin URI: http://premium.wpmudev.org/project/new-blog-template
 Description: Allows the site admin to create new blogs based on templates, to speed up the blog creation process
-Author: Jason DeVelvis, Ulrich Sossou (Incsub), Ignacio Cruz (Incsub)
+Author: WPMU DEV
 Author URI: http://premium.wpmudev.org/
 Version: 2.5.1
 Network: true
