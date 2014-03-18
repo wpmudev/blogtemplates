a9f7edd9dd3b0015b057bf769fe8041f970d2b49
ignacio <ignacio@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-03-18 08:33:00 +0000

    Added Autoblog integration
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/blogtemplates@13234 f53fe46e-8c02-459b-9fdd-93e75771b278

--- blogtemplatesfiles/integration.php
+++ blogtemplatesfiles/integration.php
@@ -74,6 +74,73 @@ function nbt_remove_blogs_from_directory( $blogs ) {
 	return $new_blogs;
 }
 
+/** AUTOBLOG **/
+add_action( 'blog_templates-copy-options', 'nbt_copy_autoblog_feeds' );
+function nbt_copy_autoblog_feeds( $template ) {
+	global $wpdb;
+
+	// Site ID, blog ID...
+	$current_site = get_current_site();
+	$current_site_id = $current_site->id;
+
+	$source_blog_id = $template['blog_id'];
+	$autoblog_on = false;
+
+	switch_to_blog( $source_blog_id );
+	// Is Autoblog activated?
+	if ( ! function_exists( 'is_plugin_active' ) )
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+	if ( is_plugin_active( 'autoblog/autoblogpremium.php' ) )
+		$autoblog_on = true;
+
+	// We'll need this values later
+	$source_url = get_site_url( $source_blog_id );
+	$source_url_ssl = get_site_url( $source_blog_id, '', 'https' );
+
+	restore_current_blog();
+
+	if ( ! $autoblog_on )
+		return;
+
+	// Getting all the feed data for the source blog ID
+	$autoblog_table = $wpdb->base_prefix . 'autoblog';
+	$results = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM $autoblog_table WHERE blog_id = %d AND site_id = %d", $source_blog_id, $current_site_id ) );
+
+	if ( ! empty( $results ) ) {
+		$current_blog_id = get_current_blog_id();
+
+		$current_url = get_site_url( $current_blog_id );
+		$current_url_ssl = get_site_url( $current_blog_id, '', 'https' );
+
+		foreach ( $results as $row ) {
+			// Getting the feed metadata
+			$feed_meta = maybe_unserialize( $row->feed_meta );
+
+			// We need to replace the source blog URL for the new one
+			$feed_meta = str_replace( $source_url, $current_url, $feed_meta );
+			$feed_meta = str_replace( $source_url_ssl, $current_url_ssl, $feed_meta );
+
+			$row->feed_meta = maybe_serialize( $feed_meta );
+
+			// Inserting feed for the new blog
+			$wpdb->insert(
+				$autoblog_table,
+				array(
+					'site_id' => $current_site_id,
+					'blog_id' => $current_blog_id,
+					'feed_meta' => $row->feed_meta,
+					'active' => $row->active,
+					'nextcheck' => $row->nextcheck,
+					'lastupdated' => $row->lastupdated
+				),
+				array( '%d', '%d', '%s', '%d', '%d', '%d' )
+			);
+		}
+	}
+
+}
+
 /** GRAVITY FORMS **/
 /*
  * Rightt now, hooking New Blog Templates into GF is not possible
