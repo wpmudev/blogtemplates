5fbedb89950fb6e89d4f804971c67eb493bf008e
ignacio <ignacio@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-05-04 07:29:16 +0000

    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/blogtemplates@14167 f53fe46e-8c02-459b-9fdd-93e75771b278

--- blogtemplates.php
+++ blogtemplates.php
@@ -29,7 +29,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-define( 'NBT_PLUGIN_VERSION', '2.6.5' );
+define( 'NBT_PLUGIN_VERSION', '2.6.6' );
 if ( ! is_multisite() )
 	exit( __( 'The New Blog Template plugin is only compatible with WordPress Multisite.', 'blog_templates' ) );
 
