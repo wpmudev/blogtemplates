3616262c27291c8c65a339f9452f8838cdbedf33
Ignacio Cruz <igmoweb@gmail.com>
2014-08-11 10:05:30 +0200

    Added WordPres HTTPS Integration

--- blogtemplatesfiles/integration.php
+++ blogtemplatesfiles/integration.php
@@ -402,3 +402,14 @@ if (defined('NBT_TO_MULTISITE_PRIVACY_ALLOW_SIGNUP_OVERRIDE') && NBT_TO_MULTISIT
 	}
 	add_filter('blog_templates_exclude_settings', 'blog_template_exclude_multisite_privacy_settings');
 }
+
+/** WORDPRESS HTTPS **/
+add_action( 'blog_templates-copy-options', 'nbt_hooks_set_https_settings' );
+function nbt_hooks_set_https_settings( $template ) {
+	if ( ! function_exists( 'is_plugin_active' ) )
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	
+	if ( is_plugin_active( 'wordpress-https/wordpress-https.php' ) )
+		update_option( 'wordpress-https_ssl_host', get_site_url( get_current_blog_id(), '', 'https' ) );
+
+}
