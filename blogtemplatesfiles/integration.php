6baa43d3894695854b59a2a3a47113099677c5aa
Ignacio Cruz <igmoweb@gmail.com>
2014-08-11 10:03:13 +0200

    Added WordPress HTTPS Integration

--- blogtemplatesfiles/integration.php
+++ blogtemplatesfiles/integration.php
@@ -299,3 +299,13 @@ function nbt_render_user_registration_form( $form_html, $form ) {
 	return $form_html;
 }
 
+/** WORDPRESS HTTPS **/
+add_action( 'blog_templates-copy-options', 'nbt_hooks_set_https_settings' );
+function nbt_hooks_set_https_settings( $template ) {
+	if ( ! function_exists( 'is_plugin_active' ) )
+		include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+	
+	if ( is_plugin_active( 'wordpress-https/wordpress-https.php' ) )
+		update_option( 'wordpress-https_ssl_host', get_site_url( get_current_blog_id(), '', 'https' ) );
+
+}
