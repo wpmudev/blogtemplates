9cbc2bddb031fbfc41b1f00b8aeba2c93e125388
Ignacio Cruz <igmoweb@gmail.com>
2016-12-19 08:23:15 +0100

    Updated DEV notification

--- blogtemplates.php
+++ blogtemplates.php
@@ -5,7 +5,7 @@ Plugin URI: http://premium.wpmudev.org/project/new-blog-template
 Description: Allows the site admin to create new blogs based on templates, to speed up the blog creation process
 Author: WPMU DEV
 Author URI: http://premium.wpmudev.org/
-Version: 2.8.3
+Version: 2.8.4
 Network: true
 Text Domain: blog_templates
 Domain Path: lang
@@ -29,7 +29,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-define( 'NBT_PLUGIN_VERSION', '2.8.3' );
+define( 'NBT_PLUGIN_VERSION', '2.8.4' );
 if ( ! is_multisite() )
 	exit( __( 'The New Blog Template plugin is only compatible with WordPress Multisite.', 'blog_templates' ) );
 
